bbcea611183170db4b6b45853ebd13da8b525ed9
joshua-james-f <joshua.james.f@gmail.com>
2015-04-04 03:12:19 -0400

    Added setup file and new clean script for MCMC tables from STAN output

---
+++ setup.R
@@ -0,0 +1,9 @@
+### Set Working Direct, Clear Environment, and Load Packages
+  setwd("/Universe/GitHub/MCMCtab")
+  source("check_packages.R")
+  check_packages(c("rstan", "xtable", "foreign", "data.table", "parallel", "dplyr", "epicalc","gmodels", "foreign", "stringr", "knitr"))
+  detachAllData( ); rm(list=ls())
+
+# Load in Trail Data and Print Parameters 
+load("/Universe/GitHub/PoliceImm_ModelsData/Data/MODsm06data.RData")
+
---
+++ tabs_STAN.R
@@ -0,0 +1,7 @@
+#####################
+# Run setup.R first 
+#####################
+
+# Temp section to display data attributes
+print(stock_m1_comp, pars = c("a", "lp__"))
+str(stock_m1_comp, max.level = 4)
